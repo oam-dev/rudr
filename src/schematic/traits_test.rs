e2211e2e1c9249b080ab450cffffc7b23dc033a4
Matt Butcher <matt.butcher@microsoft.com>
2019-06-17 16:19:23 -0600

    added extra tests

--- src/schematic/traits_test.rs
+++ src/schematic/traits_test.rs
@@ -1,10 +1,11 @@
 use k8s_openapi::apimachinery::pkg::util::intstr::IntOrString;
 
-use crate::schematic::traits;
+use crate::schematic::traits::*;
+use crate::workload_type::{REPLICATED_SERVICE_NAME, SINGLETON_NAME};
 
 #[test]
 fn test_ingress() {
-    let ig = traits::Ingress {
+    let ig = Ingress {
         name: "my-ingress".into(),
         component_name: "patsy".into(),
         svc_port: 8080,
@@ -52,7 +53,7 @@ fn test_ingress() {
 
 #[test]
 fn test_ingress_defaults() {
-    let ig = traits::Ingress {
+    let ig = Ingress {
         name: "my-ingress".into(),
         component_name: "patsy".into(),
         svc_port: 8080,
@@ -85,7 +86,7 @@ fn test_ingress_defaults() {
 
 #[test]
 fn test_autoscaler_defaults() {
-    let autoscaler = traits::Autoscaler {
+    let autoscaler = Autoscaler {
         name: "release".into(),
         component_name: "component".into(),
         cpu: None,
@@ -103,7 +104,7 @@ fn test_autoscaler_defaults() {
 
 #[test]
 fn test_autoscaler() {
-    let autoscaler = traits::Autoscaler {
+    let autoscaler = Autoscaler {
         name: "release".into(),
         component_name: "component".into(),
         cpu: Some(42),
@@ -122,9 +123,22 @@ fn test_autoscaler() {
     assert_eq!(Some(42), spec.metrics.expect("metrics")[0].clone().resource.expect("a resource").target_average_utilization);
 }
 
+
+#[test]
+fn test_ingress_workload_types() {
+    assert!(Ingress::supports_workload_type(REPLICATED_SERVICE_NAME));
+    assert!(Ingress::supports_workload_type(SINGLETON_NAME));
+}
+
+#[test]
+fn test_autoscaler_workload_types() {
+    assert!(Autoscaler::supports_workload_type(REPLICATED_SERVICE_NAME));
+    assert!(!Autoscaler::supports_workload_type(SINGLETON_NAME));
+}
+
 #[test]
 fn test_trait_labels() {
-    let labels = traits::trait_labels();
+    let labels = trait_labels();
     assert_eq!(
         "trait".to_string(),
         *labels.get("hydra.io/role").expect("must be a string")
