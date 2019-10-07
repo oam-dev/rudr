e3e7750751feb6d7197f617ee1e4a74d8b568be7
Matt Butcher <matt.butcher@microsoft.com>
2019-10-07 15:11:41 -0600

    migrated TraitManager to its own module
    Signed-off-by: Matt Butcher <matt.butcher@microsoft.com>

--- src/instigator.rs
+++ src/instigator.rs
@@ -1,11 +1,12 @@
 use failure::Error;
 use k8s_openapi::apimachinery::pkg::apis::meta::v1 as meta;
 use kube::{api::Object, api::PatchParams, api::RawApi, api::Void, client::APIClient};
-use log::{debug, error, info};
+use log::{debug, info};
 use serde_json::json;
 use std::collections::BTreeMap;
 
 use crate::{
+    trait_manager::TraitManager,
     lifecycle::Phase,
     schematic::{
         component::Component,
@@ -649,23 +650,8 @@ pub fn get_values(values: Option<Vec<ParameterValue>>) -> Vec<ParameterValue> {
     values.or_else(|| Some(vec![])).unwrap()
 }
 
-// TraitManager maps a component to its traits, and handles trait lifecycle.
-//
-// Each component configuration is assigned a trait manager. That trait manager
-// can load all of the associated traits, and then executed phases for each of
-// the traits.
-struct TraitManager {
-    config_name: String,
-    instance_name: String,
-    component: ComponentConfiguration,
-    parent_params: Vec<ParameterValue>,
-    owner_ref: Option<Vec<meta::OwnerReference>>,
-    workload_type: String,
-    // Component schematic loaded from cluster.
-    component_schematic: Component,
-    traits: Vec<HydraTrait>,
-}
 
+<<<<<<< HEAD
 impl TraitManager {
     fn load_traits(&mut self) -> Result<(), Error> {
         let mut traits: Vec<HydraTrait> = vec![];
@@ -766,3 +752,91 @@ impl TraitManager {
         Some(all_status)
     }
 }
+||||||| merged common ancestors
+impl TraitManager {
+    fn load_traits(&mut self) -> Result<(), Error> {
+        let mut traits: Vec<HydraTrait> = vec![];
+        for t in self.component.traits.as_ref().unwrap_or(&vec![]).iter() {
+            // Load all of the traits into the manager.
+            let imp = self.load_trait(&t)?;
+            traits.push(imp);
+        }
+        self.traits = traits;
+        Ok(())
+    }
+    fn load_trait(&self, binding: &TraitBinding) -> Result<HydraTrait, Error> {
+        let trait_values = resolve_values(
+            binding.parameter_values.clone().unwrap_or_else(|| vec![]),
+            self.parent_params.clone(),
+        )?;
+        debug!("Trait binding params: {:?}", &binding.parameter_values);
+        match binding.name.as_str() {
+            traits::INGRESS => {
+                let ing = Ingress::from_params(
+                    self.config_name.clone(),
+                    self.instance_name.clone(),
+                    self.component.name.clone(),
+                    trait_values,
+                    self.owner_ref.clone(),
+                );
+                Ok(HydraTrait::Ingress(ing))
+            }
+            traits::VOLUME_MOUNTER => {
+                let volmount = VolumeMounter::from_params(
+                    self.config_name.clone(),
+                    self.instance_name.clone(),
+                    self.component.name.clone(),
+                    trait_values,
+                    self.owner_ref.clone(),
+                    self.component_schematic.clone(),
+                );
+                Ok(HydraTrait::VolumeMounter(volmount))
+            }
+            traits::AUTOSCALER => {
+                let auto = Autoscaler::from_params(
+                    self.config_name.clone(),
+                    self.instance_name.clone(),
+                    self.component.name.clone(),
+                    trait_values,
+                    self.owner_ref.clone(),
+                );
+                Ok(HydraTrait::Autoscaler(auto))
+            }
+            traits::MANUAL_SCALER => {
+                let scaler = ManualScaler::from_params(
+                    self.config_name.clone(),
+                    self.instance_name.clone(),
+                    self.component.name.clone(),
+                    trait_values,
+                    self.owner_ref.clone(),
+                    self.workload_type.clone(),
+                );
+                Ok(HydraTrait::ManualScaler(scaler))
+            }
+            // Empty is a debugging tool for checking whether the traits system is functioning independently of
+            // its environment.
+            traits::EMPTY => {
+                let empty = Empty {};
+                Ok(HydraTrait::Empty(empty))
+            }
+            _ => Err(format_err!("unknown trait {}", binding.name)),
+        }
+    }
+    fn exec(&self, ns: &str, client: APIClient, phase: Phase) -> Result<(), Error> {
+        for imp in &self.traits {
+            // At the moment, we don't return an error if a trait fails.
+            let res = imp.exec(ns, client.clone(), phase.clone());
+            if res.is_err() {
+                error!(
+                    "Trait phase {:?} failed for {}: {}",
+                    phase,
+                    self.config_name.as_str(),
+                    res.unwrap_err()
+                );
+            }
+        }
+        Ok(())
+    }
+}
+=======
+>>>>>>> migrated TraitManager to its own module
