94b2620ca90250a36a69361cc6d5c1d614bf99c2
messense <messense@icloud.com>
2020-02-15 22:47:52 +0800

    Roughly port healthscope

--- healthscope/src/main.rs
+++ healthscope/src/main.rs
@@ -2,9 +2,8 @@ use chrono::{DateTime, Utc};
 use clap::{App, Arg};
 use env_logger;
 use failure::{format_err, Error};
-use futures::task::{current, Task};
-use futures::{future, Async};
-use hyper::service::{service_fn, service_fn_ok};
+use futures::future::{self, TryFutureExt};
+use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use kube::api::{ListParams, ObjectList, RawApi};
 use kube::{client::APIClient, config::incluster_config, config::load_kube_config};
@@ -15,23 +14,21 @@ use rudr::schematic::scopes::health::{
     ComponentInfo, HealthScopeObject, HealthStatus, HEALTH_SCOPE_CRD, HEALTH_SCOPE_GROUP,
     HEALTH_SCOPE_VERSION,
 };
-use std::{
-    sync::{Arc, Mutex},
-    thread,
-};
+use std::convert::Infallible;
 
 const DEFAULT_NAMESPACE: &str = "default";
 const DEFAULT_PROBE_INTERVAL: i64 = 30;
 
-fn kubeconfig() -> kube::Result<kube::config::Configuration> {
+async fn kubeconfig() -> kube::Result<kube::config::Configuration> {
     // If env var is set, use in cluster config
     if std::env::var("KUBERNETES_PORT").is_ok() {
         return incluster_config();
     }
-    load_kube_config()
+    load_kube_config().await
 }
 
-fn main() -> Result<(), Error> {
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     let flags = App::new("healthscope")
         .version(env!("CARGO_PKG_VERSION"))
         .arg(
@@ -56,11 +53,11 @@ fn main() -> Result<(), Error> {
     info!("starting server");
 
     let top_ns = std::env::var("KUBERNETES_NAMESPACE").unwrap_or_else(|_| DEFAULT_NAMESPACE.into());
-    let top_cfg = kubeconfig().expect("Load default kubeconfig");
+    let top_cfg = kubeconfig().await.expect("Load default kubeconfig");
 
     let cfg_watch = top_cfg.clone();
 
-    let health_scope_watch = std::thread::spawn(move || {
+    let health_scope_watch = tokio::spawn(async move {
         let ns = top_ns.clone();
         let healthscope_resource = RawApi::customResource("healthscopes")
             .version("v1alpha1")
@@ -69,11 +66,12 @@ fn main() -> Result<(), Error> {
         let client = APIClient::new(cfg_watch);
         let mut cnt = 0;
         loop {
-            let req = healthscope_resource.list(&ListParams::default())?;
-            match client.request::<ObjectList<HealthScopeObject>>(req) {
+            let req = healthscope_resource.list(&ListParams::default()).unwrap();
+            match client.request::<ObjectList<HealthScopeObject>>(req).await {
                 Ok(health_scopes) => {
                     for scope in health_scopes.items {
-                        if let Err(res) = aggregate_component_health(&client, scope, ns.clone()) {
+                        if let Err(res) = aggregate_component_health(&client, scope, ns.clone())
+                        .await {
                             // Log the error and continue.
                             error!("Error processing event: {:?}", res)
                         };
@@ -90,131 +88,68 @@ fn main() -> Result<(), Error> {
         }
     });
 
-    let server = std::thread::spawn(move || {
+    let server1 = tokio::spawn(async move {
         let addr = endpoint_addr.parse().unwrap();
         info!("Server is running on {}", addr);
-        hyper::rt::run(
-            Server::bind(&addr)
-                .serve(move || service_fn(serve_health))
-                .map_err(|e| eprintln!("server error: {}", e)),
-        );
+        let make_svc =
+            make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(serve_health)) });
+        Server::bind(&addr)
+            .serve(make_svc)
+            .map_err(|e| eprintln!("server error: {}", e))
     });
 
-    std::thread::spawn(move || {
+    let server2 = tokio::spawn(async move {
         let addr = metrics_addr.parse().unwrap();
         info!("Health server is running on {}", addr);
-        hyper::rt::run(
-            Server::bind(&addr)
-                .serve(|| {
-                    service_fn_ok(|_req| match (_req.method(), _req.uri().path()) {
-                        (&Method::GET, "/health") => {
-                            debug!("health check");
-                            Response::new(Body::from("OK"))
-                        }
-                        _ => Response::builder()
-                            .status(StatusCode::NOT_FOUND)
-                            .body(Body::from(""))
-                            .unwrap(),
-                    })
-                })
-                .map_err(|e| eprintln!("health server error: {}", e)),
-        );
-    })
-    .join()
-    .unwrap();
+        let make_svc =
+            make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_health)) });
+        Server::bind(&addr)
+            .serve(make_svc)
+            .map_err(|e| eprintln!("health server error: {}", e))
+    });
 
-    server.join().unwrap();
-    health_scope_watch.join().unwrap()
+    let _ = future::join3(server1, server2, health_scope_watch).await;
+    Ok(())
 }
 
-pub struct HealthFuture {
-    shared_state: Arc<Mutex<SharedState>>,
-}
 
-/// Shared state between the future and the waiting thread
-struct SharedState {
-    /// Whether or not the sleep time has elapsed
-    completed: bool,
-    resp: String,
-    task: Option<Task>,
-}
-
-impl Future for HealthFuture {
-    type Item = Response<Body>;
-    type Error = hyper::Error;
-    fn poll(&mut self) -> futures::Poll<Response<Body>, hyper::Error> {
-        // Look at the shared state to see if the timer has already completed.
-        let mut shared_state = self.shared_state.lock().unwrap();
-        if shared_state.completed {
-            Ok(Async::Ready(Response::new(Body::from(
-                shared_state.resp.clone(),
-            ))))
-        } else {
-            shared_state.task = Some(current());
-            Ok(Async::NotReady)
+async fn handle_health(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(match (_req.method(), _req.uri().path()) {
+        (&Method::GET, "/health") => {
+            debug!("health check");
+            Response::new(Body::from("OK"))
         }
-    }
+        _ => Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::from(""))
+            .unwrap(),
+    })
 }
-
-// FIXME kube-rs client doesn't support async call so we have to wrap it in HealthFuture here.
-// we could remove this wrapper until kube-rs support. https://github.com/clux/kube-rs/issues/63
-impl HealthFuture {
-    /// Create a new `TimerFuture` which will complete after the provided
-    /// timeout.
-    pub fn new(instance: String) -> Self {
-        let shared_state = Arc::new(Mutex::new(SharedState {
-            completed: false,
-            task: None,
-            resp: String::new(),
-        }));
-
-        // Spawn the new thread
-        let thread_shared_state = shared_state.clone();
-        thread::spawn(move || {
-            let res = match request_health(instance) {
-                Ok(status) => status.clone(),
-                Err(err) => {
-                    error!("{:?}", err);
-                    format!("{}", err)
-                }
-            };
-
-            let mut shared_state = thread_shared_state.lock().unwrap();
-            // Signal that the request has completed and wake up the last
-            // task on which the future was polled, if one exists.
-            shared_state.completed = true;
-            shared_state.resp = res;
-            if let Some(ref task) = shared_state.task {
-                task.notify()
-            }
-        });
-
-        HealthFuture { shared_state }
-    }
-}
-
-type BoxFut = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
 // serve_health make health scope controller as an http server, it will serve requests and get the real health status from health scope instance
-fn serve_health(req: Request<Body>) -> BoxFut {
+async fn serve_health(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let mut response = Response::new(Body::empty());
     let path = req.uri().path().to_owned();
     match (req.method(), path) {
         (&Method::GET, path) => {
             let instance = path.trim_start_matches('/').to_string();
             info!("{} health scope requested", instance);
-            return Box::new(HealthFuture::new(instance));
+            let info = match request_health(instance).await {
+                Ok(health) => health,
+                Err(e) => format!("{}", e),
+            };
+            return Ok(Response::new(Body::from(info)));
         }
         _ => *response.status_mut() = StatusCode::NOT_FOUND,
     }
-    Box::new(future::ok(response))
+    Ok(response)
 }
 
 // request_health will request health scope instance CR and get status from the CR object
-fn request_health(instance_name: String) -> Result<String, Error> {
+async fn request_health(instance_name: String) -> Result<String, Error> {
     let namespace =
         std::env::var("KUBERNETES_NAMESPACE").unwrap_or_else(|_| DEFAULT_NAMESPACE.into());
-    let cfg = kubeconfig().unwrap();
+    let cfg = kubeconfig().await.unwrap();
     println!(
         "cfg {:?}, instance {}",
         cfg.base_path.clone(),
@@ -227,7 +162,7 @@ fn request_health(instance_name: String) -> Result<String, Error> {
         .group("core.oam.dev")
         .within(namespace.as_str());
     let req = healthscope_resource.get(instance_name.as_str())?;
-    let obj = client.request::<HealthScopeObject>(req)?;
+    let obj = client.request::<HealthScopeObject>(req).await?;
     let mut health = "healthy";
     obj.status.map(|status| {
         status.clone().components.map(|comps| {
@@ -243,7 +178,7 @@ fn request_health(instance_name: String) -> Result<String, Error> {
     Ok(health.to_string())
 }
 
-fn aggregate_component_health(
+async fn aggregate_component_health(
     client: &APIClient,
     mut event: HealthScopeObject,
     namespace: String,
@@ -258,20 +193,18 @@ fn aggregate_component_health(
         event.spec.probe_endpoint.as_str(),
     ) {
         ("kube-get", ".status") => {
-            let components =
-                event
-                    .status
-                    .and_then(|status| status.components)
-                    .and_then(|mut components| {
-                        for c in components.iter_mut() {
-                            c.status = Some(get_health_from_component(
-                                client,
-                                c.clone(),
-                                namespace.clone(),
-                            ))
-                        }
-                        Some(components)
-                    });
+            let components = if let Some(mut components) = event.status.and_then(|status| status.components) {
+                for c in components.iter_mut() {
+                    c.status = Some(get_health_from_component(
+                        client,
+                        c.clone(),
+                        namespace.clone(),
+                    ).await)
+                }
+                Some(components)
+            } else {
+                None
+            };
             event.status = Some(HealthStatus {
                 components,
                 last_aggregate_timestamp: Some(Utc::now().to_rfc3339()),
@@ -286,7 +219,7 @@ fn aggregate_component_health(
                 &pp,
                 serde_json::to_vec(&event)?,
             )?;
-            client.request::<HealthScopeObject>(req)?;
+            client.request::<HealthScopeObject>(req).await?;
             Ok(())
         }
         _ => Err(format_err!(
@@ -297,14 +230,14 @@ fn aggregate_component_health(
     }
 }
 
-fn get_health_from_component(client: &APIClient, info: ComponentInfo, namespace: String) -> String {
+async fn get_health_from_component(client: &APIClient, info: ComponentInfo, namespace: String) -> String {
     let name = combine_name(info.name, info.instance_name);
     let crd_req = RawApi::customResource("componentinstances")
         .group(CONFIG_GROUP)
         .version(CONFIG_VERSION)
         .within(namespace.as_str());
     let req = crd_req.get(name.as_str()).unwrap();
-    let res: KubeComponentInstance = match client.request(req) {
+    let res: KubeComponentInstance = match client.request(req).await {
         Ok(ins) => ins,
         Err(e) => {
             error!("get component instance failed {:?}", e);
